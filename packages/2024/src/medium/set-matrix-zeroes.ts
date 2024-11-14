b8385fe0dd63c07ec520b7cf3268fa0158bfe0da
RYAN WAY <ryankway@amazon.com>
2024-11-14 11:43:06 -0800

    Set Matrix Zeres using constanst space

--- packages/2024/src/medium/set-matrix-zeroes.ts
+++ packages/2024/src/medium/set-matrix-zeroes.ts
@@ -1,21 +1,34 @@
 export function setZeroes(matrix: number[][]): void {
-  const rows = new Set();
-  const cols = new Set();
-
+  let setFirstRow = false;
+  let setFirstCol = false;
   for (let row = 0; row < matrix.length; row++) {
     for (let col = 0; col < matrix[row].length; col++) {
       if (matrix[row][col] === 0) {
-        rows.add(row);
-        cols.add(col);
+        setFirstRow ||= row === 0;
+        setFirstCol ||= col === 0;
+        matrix[row][0] = 0;
+        matrix[0][col] = 0;
       }
     }
   }
 
-  for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix[row].length; col++) {
-      if (rows.has(row) || cols.has(col)) {
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[row].length; col++) {
+      if (matrix[row][0] === 0 || matrix[0][col] === 0) {
         matrix[row][col] = 0;
       }
+    }
+  }
+
+  if (setFirstRow) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      matrix[0][col] = 0;
+    }
+  }
+
+  if (setFirstCol) {
+    for (const row of matrix) {
+      row[0] = 0;
     }
   }
 }
